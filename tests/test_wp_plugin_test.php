a86ba10cc0a7c7b77e3398330e10a70ecac7628a
Mikko Lauhakari <webmaster@lauhakari.com>
2013-06-26 20:58:10 +0200

    switch to blog test id to id

--- tests/test_wp_plugin_test.php
+++ tests/test_wp_plugin_test.php
@@ -15,9 +15,11 @@
 
 			$blog_id = $this->factory->blog->create(array('path' => '/test_blogpath', 'title' => 'Test Title' ));
 
+			switch_to_blog( $blog_id );
+			$current_blog_id = get_current_blog_id();
 
-
-			$this->assertTrue(switch_to_blog( $blog_id ));
+			$this->assertEquals($current_blog_id, $blog_id);
+			//$this->assertTrue();
 		}
 
 	}
