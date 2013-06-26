5ea8a61a4edd664043b460e70acdf9f1775c4f57
Mikko Lauhakari <webmaster@lauhakari.com>
2013-06-26 18:47:49 +0200

    test that tests work

--- tests/test_wp_plugin_test.php
+++ tests/test_wp_plugin_test.php
@@ -9,9 +9,11 @@ class WP_Plugin_Test extends WP_UnitTestCase {
 
 
 	function test_change_to_blog() {
-		$this->factory->blog->create_many( 3 );
+/*		$this->factory->blog->create_many( 3 ); */
 
 /*		$this->assertTrue(switch_to_blog(2)); */
+
+		$this->assertTrue(TRUE);
 	}
 
 }
