0579724568f49e16f6843eb84768b8e2ddc76f4d
Mikko Lauhakari <webmaster@lauhakari.com>
2013-06-26 19:40:06 +0200

    test get post from wp-test-unit

--- tests/test_wp_plugin_test.php
+++ tests/test_wp_plugin_test.php
@@ -1,37 +1,35 @@
-<?php
+	<?php
 
-/**
- * Tests for the WP Plugin Test plugin.
- *
- * @package wp-plugin-test
- */
-class WP_Plugin_Test extends WP_UnitTestCase {
+	/**
+	 * Tests for the WP Plugin Test plugin.
+	 *
+	 * @package wp-plugin-test
+	 */
+	class WP_Plugin_Test extends WP_UnitTestCase {
 
-	function setUp() {
-		parent::setUp();
+		function setUp() {
+			parent::setUp();
+		}
+
+		function test_the_content() {
+			$post_content = "EOF
+			<i>This is the excerpt.</i>
+			<!--more-->
+			This is the <b>body</b>.";
+
+			$post_id = $this->factory->post->create( compact( 'post_content' ) );
+
+			$expected = "
+			<p><i>This is the excerpt.</i><br />
+			<span id="more-{$post_id}"></span><br />
+			This is the <b>body</b>.</p>";
+
+			$this->go_to( get_permalink( $post_id ) );
+			$this->assertTrue( is_single() );
+			$this->assertTrue( have_posts() );
+			$this->assertNull( the_post() );
+
+			$this->assertEquals( strip_ws( $expected ), strip_ws( get_echo( 'the_content' ) ) );
+		}
+
 	}
-
-function test_the_content() {
-		$post_content = <<<EOF
-<i>This is the excerpt.</i>
-<!--more-->
-This is the <b>body</b>.
-EOF;
-
-		$post_id = $this->factory->post->create( compact( 'post_content' ) );
-
-		$expected = <<<EOF
-<p><i>This is the excerpt.</i><br />
-<span id="more-{$post_id}"></span><br />
-This is the <b>body</b>.</p>
-EOF;
-
-		$this->go_to( get_permalink( $post_id ) );
-		$this->assertTrue( is_single() );
-		$this->assertTrue( have_posts() );
-		$this->assertNull( the_post() );
-
-		$this->assertEquals( strip_ws( $expected ), strip_ws( get_echo( 'the_content' ) ) );
-	}
-
-}
