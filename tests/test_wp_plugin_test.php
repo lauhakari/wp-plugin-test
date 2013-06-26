d3c20f900d0380e254fff7348c9350aa13d2934a
Mikko Lauhakari <webmaster@lauhakari.com>
2013-06-26 19:12:11 +0200

    test get post

--- tests/test_wp_plugin_test.php
+++ tests/test_wp_plugin_test.php
@@ -11,18 +11,19 @@ class WP_Plugin_Test extends WP_UnitTestCase {
 		parent::setUp();
 	}
 
-
-
-
-	function test_change_to_blog() {
+function test_change_to_blog() {
 
 		global $wpdb;
 
+
+
+		$this->assertObjectHasAttribute('post_date', get_post(1);
+/*
 		$blog_ids = $this->factory->blog->create_many( 4 );
 		foreach ( $blog_ids as $blog_id ) {
 			$this->assertTrue(switch_to_blog($blog_id));
 		}
-
+*/
 	}
 
 }
